6674dd101fc74d02e05b888c5fa2cb0ed32cc469
YoussefWilliam <youssef.william997@gmail.com>
2021-07-10 20:36:49 +0200

    Adding features section

--- src/sections/key-feature.js
+++ src/sections/key-feature.js
@@ -1,16 +1,39 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid } from 'theme-ui';
-import SectionHeader from '../components/section-header';
-import FeatureCardColumn from 'components/feature-card-column.js';
-import Performance from 'assets/key-feature/performance.svg';
-import Partnership from 'assets/key-feature/partnership.svg';
-import Subscription from 'assets/key-feature/subscription.svg';
-import Support from 'assets/key-feature/support.svg';
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "../components/section-header";
+import FeatureCardColumn from "components/feature-card-column.js";
+import Performance from "assets/key-feature/performance.svg";
+import Partnership from "assets/key-feature/partnership.svg";
+import Subscription from "assets/key-feature/subscription.svg";
+import Support from "assets/key-feature/support.svg";
+import { keyFeatureStyles } from "./styles";
+import { keyFeatureData } from "./data";
 
-
-export default function KeyFeature() {
+const KeyFeature = () => {
   return (
-   <h1>Key Feature</h1>
+    <section id="feature" sx={{ variant: "section.keyFeature" }}>
+      <Container>
+        <SectionHeader
+          slogan="What's the function"
+          title="Meet the feature of our product"
+        />
+        <Grid sx={keyFeatureStyles.grid}>
+          {keyFeatureData.map((item, i) => {
+            return (
+              <FeatureCardColumn
+                key={`${i} + ${item.id}`}
+                src={item.imgSrc}
+                alt={item.altText}
+                title={item.title}
+                text={item.text}
+              />
+            );
+          })}
+        </Grid>
+      </Container>
+    </section>
   );
-}
+};
+
+export default KeyFeature;
