419de5c268b6c37bf6bdb407ec70d604ad769517
YoussefWilliam <youssef.william997@gmail.com>
2021-07-10 19:55:04 +0200

    Adding banner

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,20 +1,27 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Box, Heading, Text, Image, Button } from 'theme-ui';
-import {} from "./data"
-import {bannerStyles} from "./styles"
-
+import { jsx } from "theme-ui";
+import { Container, Box, Heading, Text, Image, Button } from "theme-ui";
+import {} from "./data";
+import { bannerStyles } from "./styles";
+import BannerImg from "assets/banner-thumb.png";
 export default function Banner() {
   return (
     <section id="home" sx={bannerStyles.banner}>
       <Container sx={bannerStyles.banner.container}>
         <Box sx={bannerStyles.banner.contentBox}>
           <Heading as="h1" variant="heroPrimary">
-            Hu ther
+            Head towards something great
           </Heading>
+          <Text as="p" variant="heroSecondary">
+            Check our new website to start having same experience If you need
+            this for yourself, this is the place to go!
+          </Text>
+          <Button variant="primary">Explore</Button>
+        </Box>
+        <Box sx={bannerStyles.banner.imageBox}>
+          <Image src={BannerImg} />
         </Box>
       </Container>
     </section>
   );
 }
-
