2800589afa7ff3ede4fbc200d86fc818667128fa
YoussefWilliam <youssef.william997@gmail.com>
2021-07-20 22:42:07 +0200

    Adding workflow section

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -1,16 +1,39 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid, Box, Heading, Text } from 'theme-ui';
-import SectionHeader from 'components/section-header';
+import { jsx } from "theme-ui";
+import { Container, Grid, Box, Heading, Text } from "theme-ui";
+import SectionHeader from "components/section-header";
 
-import PatternBG from 'assets/patternBG.png';
-import ArrowOdd from 'assets/arrowOdd.svg';
-import ArrowEven from 'assets/arrowEven.svg';
+import PatternBG from "assets/patternBG.png";
+import ArrowOdd from "assets/arrowOdd.svg";
+import ArrowEven from "assets/arrowEven.svg";
+import { workflowStyles } from "./styles";
+import { workflowData } from "./data";
 
-
-
-export default function WorkFlow() {
+const WorkFlow = () => {
   return (
-    <h1>Workflow</h1>
+    <section sx={workflowStyles.workflow}>
+      <Container>
+        <SectionHeader
+          slogan="What is our workflow"
+          title="Meet the features of our products"
+          isWhite
+        />
+        <Grid sx={workflowStyles.grid}>
+          {workflowData.map((data) => (
+            <Box sx={workflowStyles.card} key={data.id}>
+              <Box sx={workflowStyles.iconBox}>{`0${data.id}`} </Box>
+              <Box sx={workflowStyles.wrapper}>
+                <Heading sx={workflowStyles.wrapper.title}>
+                  {data.title}
+                </Heading>
+                <Text sx={workflowStyles.wrapper.subTitle}>{data.text} </Text>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
-}
+};
+
+export default WorkFlow;
