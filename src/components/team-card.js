48eeab91ff712b4d4912c3f9e6551ca557d525fb
YoussefWilliam <youssef.william997@gmail.com>
2021-07-21 10:52:07 +0200

    Adding teams

--- src/components/team-card.js
+++ src/components/team-card.js
@@ -3,9 +3,25 @@ import { jsx } from "theme-ui";
 import { Text, Heading, Image, Box, Link } from "theme-ui";
 import { teamCardStyles } from "./styles";
 
-
 const TeamCard = ({ src, altText, title, designation, social }) => {
-  return <h1>TeamCard</h1>;
+  return (
+    <Box sx={teamCardStyles.card}>
+      <Image src={src} alt={altText} sx={teamCardStyles.memberThumb} />
+      <Box sx={teamCardStyles.infoWrapper}>
+        <Heading className="info__name" sx={teamCardStyles.infoWrapper}>
+          {title}
+        </Heading>
+        <Text className="info__designation" sx={teamCardStyles.designation}>
+          {designation}
+        </Text>
+      </Box>
+      <Box sx={teamCardStyles.socialShare} className="social__share">
+        {social.map((item) => (
+          <Link key={item.id} href={item.path} className={item.name}>{item.icon}</Link>
+        ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default TeamCard;
--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -1,21 +1,39 @@
 /** @jsx jsx */
-import { jsx } from 'theme-ui';
-import { Container, Grid } from 'theme-ui';
-import SectionHeader from 'components/section-header';
-import TeamCard from 'components/team-card';
-import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { jsx } from "theme-ui";
+import { Container, Grid } from "theme-ui";
+import SectionHeader from "components/section-header";
+import TeamCard from "components/team-card";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
 
-import Member1 from 'assets/team/member-1.png';
-import Member2 from 'assets/team/member-2.png';
-import Member3 from 'assets/team/member-3.png';
-import Member4 from 'assets/team/member-4.png';
-import Member5 from 'assets/team/member-5.png';
-import Member6 from 'assets/team/member-6.png';
+import Member1 from "assets/team/member-1.png";
+import Member2 from "assets/team/member-2.png";
+import Member3 from "assets/team/member-3.png";
+import Member4 from "assets/team/member-4.png";
+import Member5 from "assets/team/member-5.png";
+import Member6 from "assets/team/member-6.png";
 
+import { teamSectionData } from "./data";
+import { teamSectionStyles } from "./styles";
 
-export default function TeamSection() {
+const TeamSection = () => {
   return (
-    <h1>Team Section</h1>
+    <section>
+      <Container>
+        <SectionHeader slogan="Our Team" title="World class individuals" />
+        <Grid sx={teamSectionStyles.grid}>
+          {teamSectionData.map((item) => (
+            <TeamCard
+              src={item.imgSrc}
+              title={item.title}
+              key={item.id}
+              altText={item.altText}
+              designation={item.designation}
+              social={item.socialProfile}
+            />
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
-}
-
+};
+export default TeamSection;
